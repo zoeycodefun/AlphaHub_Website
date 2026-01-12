215641118bbcc6e170898fefb9dad80e207b2f31
zoeycodefun <zoeycodefun@gmail.com>
2026-01-12 22:59:33 +0800

    commit change in 2026.1.12

--- src/layout/layout_components/user_accounts.tsx
+++ src/layout/layout_components/user_accounts.tsx
@@ -1,5 +1,11 @@
 // 用户账户管理（顶栏账户管理是所有账户）
-import React, {useState} from 'react';
+import React, {useState, useEffect, } from 'react';
+import { X, Plus, Link, Settings, Trash2} from 'lucide-react'
 /**
  * 用户管理组件：用户账户管理的弹窗，显示用户在此系统，此市场的所有账户，按照账户类型分类
  */
+// 类型定义
+interface Exchange {
+    id: string;
+    
+}
